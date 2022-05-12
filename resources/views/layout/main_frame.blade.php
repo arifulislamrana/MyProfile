afb36520f33bbe008ba9c10ba178b0a996282174
arifulislamrana <ariful.islam0683@gmail.com>
2022-05-13 01:07:56 +0600

    Basic setup with Front view

---
+++ resources/views/layout/main_frame.blade.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<!-- Mirrored from lmpixels.com/demo/breezycv/dark/1/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 11 Mar 2022 17:38:02 GMT -->
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>BreezyCV - Resume / CV / vCard Template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="description" content="BreezyCV - Resume / CV / vCard Template" />
+  <meta name="keywords" content="vcard, resposnive, retina, resume, jquery, css3, bootstrap, Material CV, portfolio" />
+  <meta name="author" content="lmpixels" />
+  <link rel="shortcut icon" href="favicon.ico">
+
+
+  <link rel="stylesheet" href="Assets/css/reset.css" type="text/css">
+  <link rel="stylesheet" href="Assets/css/bootstrap-grid.min.css" type="text/css">
+  <link rel="stylesheet" href="Assets/css/animations.css" type="text/css">
+  <link rel="stylesheet" href="Assets/css/perfect-scrollbar.css" type="text/css">
+  <link rel="stylesheet" href="Assets/css/owl.carousel.css" type="text/css">
+  <link rel="stylesheet" href="Assets/css/magnific-popup.css" type="text/css">
+  <link rel="stylesheet" href="Assets/css/main.css" type="text/css">
+
+  <script src="Assets/js/modernizr.custom.js"></script>
+</head>
+
+<body>
+  <!-- Animated Background -->
+  <div class="lm-animated-bg" style="background-image: url(img/main_bg.png);"></div>
+  <!-- /Animated Background -->
+
+  <!-- Loading animation -->
+  <div class="preloader">
+    <div class="preloader-animation">
+      <div class="preloader-spinner">
+      </div>
+    </div>
+  </div>
+  <!-- /Loading animation -->
+
+  <div class="page">
+    <div class="page-content">
+
+      <header id="site_header" class="header mobile-menu-hide">
+        <div class="header-content">
+          <div class="header-photo">
+            <img src="Assets/img/main_photo.jpg" alt="Alex Smith">
+          </div>
+          <div class="header-titles">
+            <h2>Ariful Islam</h2>
+            <h4>Full stack developer</h4>
+          </div>
+        </div>
+
+        <ul class="main-menu">
+          <li class="active">
+            <a href="#home" class="nav-anim">
+              <span class="menu-icon lnr lnr-home"></span>
+              <span class="link-text">Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#about-me" class="nav-anim">
+              <span class="menu-icon lnr lnr-user"></span>
+              <span class="link-text">About Me</span>
+            </a>
+          </li>
+          <li>
+            <a href="#resume" class="nav-anim">
+              <span class="menu-icon lnr lnr-graduation-hat"></span>
+              <span class="link-text">Resume</span>
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" class="nav-anim">
+              <span class="menu-icon lnr lnr-briefcase"></span>
+              <span class="link-text">Portfolio</span>
+            </a>
+          </li>
+          <li>
+            <a href="#blog" class="nav-anim">
+              <span class="menu-icon lnr lnr-book"></span>
+              <span class="link-text">Blog</span>
+            </a>
+          </li>
+          <li>
+            <a href="#contact" class="nav-anim">
+              <span class="menu-icon lnr lnr-envelope"></span>
+              <span class="link-text">Contact</span>
+            </a>
+          </li>
+        </ul>
+
+        <div class="social-links">
+          <ul>
+            <li><a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+          </ul>
+        </div>
+
+        <div class="header-buttons">
+          <a href="#" target="_blank" class="btn btn-primary">Download CV</a>
+        </div>
+
+        <div class="copyrights">© 2020 All rights reserved.</div>
+      </header>
+
+      <!-- Mobile Navigation -->
+      <div class="menu-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <!-- End Mobile Navigation -->
+
+      <!-- Arrows Nav -->
+      <div class="lmpixels-arrows-nav">
+        <div class="lmpixels-arrow-right"><i class="lnr lnr-chevron-right"></i></div>
+        <div class="lmpixels-arrow-left"><i class="lnr lnr-chevron-left"></i></div>
+      </div>
+      <!-- End Arrows Nav -->
+
+      <div class="content-area">
+        <div class="animated-sections">
+          @yield('content')
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <script data-cfasync="false" src="../../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js">
+  </script>
+  <script src="Assets/js/jquery-2.1.3.min.js"></script>
+  <script src="Assets/js/modernizr.custom.js"></script>
+  <script src="Assets/js/animating.js"></script>
+
+  <script src="Assets/js/imagesloaded.pkgd.min.js"></script>
+  <script src='../../../../../www.google.com/recaptcha/api.js'></script>
+
+  <script src='js/perfect-scrollbar.min.js'></script>
+  <script src='js/jquery.shuffle.min.js'></script>
+  <script src='js/masonry.pkgd.min.js'></script>
+  <script src='js/owl.carousel.min.js'></script>
+  <script src="Assets/js/jquery.magnific-popup.min.js"></script>
+
+  <script src="Assets/js/validator.js"></script>
+  <script src="Assets/js/main.js"></script>
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194"
+    integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw=="
+    data-cf-beacon='{"rayId":"6ea60fb41b464565","version":"2021.12.0","r":1,"token":"94b99c0576dc45bf9d669fb5e9256829","si":100}'
+    crossorigin="anonymous"></script>
+</body>
+
+<!-- Mirrored from lmpixels.com/demo/breezycv/dark/1/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 11 Mar 2022 17:38:48 GMT -->
+
+</html>
